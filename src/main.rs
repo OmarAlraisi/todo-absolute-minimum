4510f6040b1cbbf0bb3cef878595e3dc94d5f70a
AlRaisi96 <o.raisi@hotmail.com>
2024-04-07 14:32:54 +0400

    starts parsing the request

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,43 @@ use std::net::TcpListener;
 
 const BUFFER_SIZE: usize = 1024;
 
+#[derive(Debug)]
+enum ContentType {
+    TextPlain,
+    ApplicationJson,
+}
+
+#[derive(Debug)]
+struct Request {
+    method: String,
+    resource: String,
+    content_type: ContentType,
+    body: String,
+}
+
+impl Request {
+    fn new(raw: String) -> Self {
+        println!("{raw}");
+        let mut lines = raw.lines();
+        let mut first_line = lines.next().expect("Invalid HTTP request!").split_whitespace();
+        let method = first_line.next().expect("Missing Method name").to_string();
+        let resource = first_line.next().expect("Missing resource name").to_string();
+        let mut content_type = ContentType::TextPlain;
+        while let Some(line) = lines.next() {
+            let line = line.to_lowercase();
+            let mut line = line.split(':');
+            if let Some("content-type") = line.next() {
+                let content = line.next().unwrap().trim();
+                content_type = match content {
+                    "text/plain" => ContentType::TextPlain,
+                    _ => ContentType::ApplicationJson,
+                }
+            }
+        }
+        Request { method, resource, content_type, body: String::new() }
+    }
+}
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").expect("Couldn't start server on port 8080");
     // Intercept streams
@@ -23,8 +60,8 @@ fn main() {
                     break;
                 }
             }
-            let request = String::from_utf8(buffer).unwrap();
-            println!("{}", request);
+            let request = Request::new(String::from_utf8(buffer).unwrap());
+            println!("{:?}", request);
 
 
             let mut writer = BufWriter::new(stream);
